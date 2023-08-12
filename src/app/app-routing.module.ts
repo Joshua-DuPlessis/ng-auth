053b08912b4d8382e5b9e7a4b358c5bb08196b8a
Joshua <joshua1duplessis@gmail.com>
2023-08-12 13:33:09 +0200

    ng-frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserlistingComponent} from "./userlisting/userlisting.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:LoginComponent,path:'login'},
+  {component:RegisterComponent,path:'register'},
+  {component:HomeComponent,path:'',canActivate:[AuthGuard]},
+  {component:UserlistingComponent,path: 'UserListingComponent'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
